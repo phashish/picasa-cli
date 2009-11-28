d038e9b9dd7bb5ba8793e0bafd96519cdfea40ae
ashish.disawal <ashish.disawal@0d386b3c-d193-11de-aacf-132551bdafba>
2009-11-28 08:09:06 +0000

    Added local dir cd, ls commands

--- trunk/picasa-cli/tests/cli.py
+++ trunk/picasa-cli/tests/cli.py
@@ -1,51 +1,27 @@
 #!/usr/bin/python
 #
-# picasa-cli
-# Ashish Disawal
-# I have modified the google docs example, available at:
-# http://gdata-python-client.googlecode.com/svn/trunk/samples/docs/docs_example.py (r882)
-# which was licensed under the Apache License, Version 2.0.
-# I have licensed this code under GPL v2.
-# Thanks to Jeff Fisher and Eric Bidelman for docs_example.py
-# For more info go to : http://ashish.disawal.blogspot.com
-
 import re
 import sys
 import getopt
 import getpass
 import os.path
-import gdata.geo
-import gdata.media
-import gdata.photos.service
 
-class PicasaCli(object):
-	"""A picasa web photo list feed."""
+class Cli(object):
 
-	def __init__(self, email, password):
-		"""Constructor for the PicasaCli object.
-		Takes an email and password corresponding to a gmail account.
-		Args:
-		  email: [string] The e-mail address of the account to use for the sample.
-		  password: [string] The password corresponding to the account specified by
-		  the email parameter.
-		Returns:
-		  A PicasaCli object.
-		"""
-		source = 'PicasaCli'
-		self.gd_client = gdata.photos.service.PhotosService()
-		self.gd_client.ClientLogin(email, password, source=source)
-
+	def __init__(self, user):
+		self.user = user
 	def _GetCmd(self):
 		while True:
 			input = raw_input('> ')
+			print input
 			return str(input)
 
 	def _PrintHelp(self):
 		"""Displays help of commands for the user to choose from."""
 		print ('\nCurrently supported commands:\n'
-			'help, quit, lcwd, lcd, lls, ls, cd\n'
+			'help, quit, lcwd, lcd\n'
 			"Commands yet to be implimented:\n"
-			'list, mkalbm, put, get .. and many more\n')
+			'ls, mkdir, cd, put, get .. and many more\n')
 
 	def _LocalCWD(self):
 		print os.getcwd()
@@ -57,7 +33,7 @@ class PicasaCli(object):
 			print "No such directory or you don't have permissions to access it."
 			return
 		print "Current local dir: ", os.getcwd()
-
+	
 	def _LocalList(self, dir):
 		for dirname in os.listdir(dir):
 			print dirname
@@ -69,7 +45,6 @@ class PicasaCli(object):
 		print "List of photos in ", albumid
 
 	def Run(self):
-		"""Prompts the user to choose funtionality to be demonstrated."""
 		try:
 			while True:
 				cmd = self._GetCmd()
@@ -83,7 +58,7 @@ class PicasaCli(object):
 					self._LocalCD(dir)
 				elif re.match("^lls", cmd):
 					dir = re.findall(r'^lls (\S+)', cmd)[0]
-					self._LocalList(dirs)
+					self._LocalList(dir)
 				# Commands for picasaweb
 				elif cmd == 'lsalbm':
 					self._ListAlbums()
@@ -99,38 +74,34 @@ class PicasaCli(object):
 			return
 
 def main():
-	"""FTP like cli for picasa web albums."""
 	# Parse command line options
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], '', ['user=', 'pw='])
+		opts, args = getopt.getopt(sys.argv[1:], '', ['user='])
 	except getopt.error, msg:
-		print 'python picasa-cli.py --user [username] --pw [password] '
+		print 'python picasa-cli.py --user [username] '
 		sys.exit(2)
 
 	user = ''
-	pw = ''
 	key = ''
 	# Process options
 	for option, arg in opts:
 		if option == '--user':
 			user = arg
-		elif option == '--pw':
-			pw = arg
 
 	while not user:
 		user = raw_input('Please enter your username: ')
-	while not pw:
-		pw = getpass.getpass()
-		if not pw:
-			print 'Password cannot be blank.'
+#	while not pw:
+#		pw = getpass.getpass()
+#		if not pw:
+#			print 'Password cannot be blank.'
 
 	try:
-		cli = PicasaCli(user, pw)
+		cli = Cli(user)
 	except gdata.service.BadAuthentication:
 		print 'Invalid user credentials given.'
 		return
 
-	print ("Successfully logged in to Picasa Web.\n"
+	print ("Successfully logged in.\n"
 		   "Type 'help' for list of available commands.\n")
 	cli.Run()
 
